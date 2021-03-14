eca35c87db813dbb79f5dd056948d1b2fdc09d14
Farhan Alam <metalhead901@live.com>
2021-03-14 23:05:27 +0600

    added bootstrap css

--- src/App.js
+++ src/App.js
@@ -2,8 +2,13 @@ import './styles/styles.scss';
 
 function App() {
 	return (
-		<div className="container">
-			dfd
+		<div className="container d-flex flex-column h-100 py-3">
+			<h1 className='text-center form-group'>
+				Budget Planner
+			</h1>
+			<div className="text-center mt-auto form-text text-muted">
+				<small>Created by <a href="https://github.com/fa-901" rel='noopener noreferrer' target="_blank">fa-901</a></small>
+			</div>
 		</div>
 	);
 }
