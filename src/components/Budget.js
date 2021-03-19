b90690b84cbdf52bfab7d01a979bda07efe34325
Farhan Alam <metalhead901@live.com>
2021-03-19 12:00:25 +0600

    toggle edit view in budget

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -1,3 +1,4 @@
+import { useState, useRef } from 'react';
 import {
     setAmount,
     selectCount,
@@ -9,12 +10,26 @@ import { useSelector, useDispatch } from 'react-redux'
 export default function Budget(props) {
     const value = useSelector(selectCount);
     const dispatch = useDispatch();
+    const [isEdit, switchEdit] = useState(false);
+    const [input, handleInput] = useState(value);
+    const inputRef = useRef(null);
 
+    function toggleEdit() {
+        switchEdit(p => !p);
+    }
+
+    var display = isEdit ? (
+        <input type='number' className='num-input' value={input} onChange={(e) => { handleInput(e.currentTarget.value) }} onBlur={toggleEdit} ref={inputRef} />
+    ) : `Budget: $${value}`;
 
     return (
-        <div className='budget'>
-            Budget: ${value}
-            <div></div>
+        <div className='budget d-flex align-items-center'>
+            <div className='me-3'>
+                {display}
+            </div>
+            <button className='ms-auto' onClick={toggleEdit} >
+                <i className={`far fa-${isEdit ? 'save' : 'edit'}`}></i>
+            </button>
         </div>
     )
 }
